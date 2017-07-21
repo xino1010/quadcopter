9f096ec277610fe9b8dbdc60176d499812948375
Daniel Andújar <daniandujar@gmail.com>
2017-07-21 16:41:37 +0200

    Transmitter using 2 joysticks

---
+++ transmitter/transmitter.ino
@@ -0,0 +1,109 @@
+#define DEBUG
+#define JOYSTICK1
+#define JOYSTICK2
+#define MIN_ANALOG_VALUE 0
+#define MEDIUM_ANALOG_VALUE 512
+#define MAX_ANALOG_VALUE 1023
+#define MIN_THROTTLE 1000
+#define MAX_THROTTLE 2000
+#define MIN_PITCH -45
+#define MAX_PITCH 45
+#define MIN_ROLL -45
+#define MAX_ROLL 45
+#define MIN_YAW -30
+#define MAX_YAW 30
+
+struct Joystick {
+  int num;
+  int pinX;
+  int pinY;
+  int pinButton;
+  int valX;
+  int valY;
+  int valButton;
+};
+
+Joystick j1 = {1, A0, A1, 7, MEDIUM_ANALOG_VALUE, 0, 0};
+Joystick j2 = {2, A2, A3, 8, MEDIUM_ANALOG_VALUE, MEDIUM_ANALOG_VALUE, 0};
+
+struct SetPoints {
+  int throttle;
+  double pitch;
+  double roll;
+  double yaw;
+};
+
+SetPoints sp = {1000, 0.0, 0.0, 0.0};
+
+void readJoystick1() {
+  j1.valX = analogRead(j1.pinX);
+  j1.valY = MAX_ANALOG_VALUE - analogRead(j1.pinY);
+  j1.valButton = digitalRead(j1.pinButton);
+}
+
+void readJoystick2() {
+  j2.valX = analogRead(j2.pinX);
+  j2.valY = analogRead(j2.pinY);
+  j2.valButton = digitalRead(j2.pinButton);
+}
+
+void printJoystickData(struct Joystick *j) {
+  Serial.print("JOYSTICK ");
+  Serial.print(j->num);
+  Serial.print(" -> X: ");
+  Serial.print(j->valX);
+  Serial.print(" Y: ");
+  Serial.print(j->valY);
+  Serial.print(" BUTTON: ");
+  Serial.println(j->valButton);
+}
+
+void calculateSetpoints() {
+  sp.throttle = constrain(map(j1.valY, MEDIUM_ANALOG_VALUE, MAX_ANALOG_VALUE, MIN_THROTTLE, MAX_THROTTLE), MIN_THROTTLE, MAX_THROTTLE);
+  sp.pitch = constrain(map(j2.valY, MIN_ANALOG_VALUE, MAX_ANALOG_VALUE, MIN_PITCH, MAX_PITCH), MIN_PITCH, MAX_PITCH);
+  sp.roll = constrain(map(j2.valX, MIN_ANALOG_VALUE, MAX_ANALOG_VALUE, MIN_ROLL, MAX_ROLL), MIN_ROLL, MAX_ROLL);
+  sp.yaw = constrain(map(j1.valX, MIN_ANALOG_VALUE, MAX_ANALOG_VALUE, MIN_YAW, MAX_YAW), MIN_YAW, MAX_YAW);
+}
+
+void printSetpoints() {
+  Serial.print("Throttle: ");
+  Serial.print(sp.throttle);
+  Serial.print(" Pitch: ");
+  Serial.print(sp.pitch);
+  Serial.print(" Roll: ");
+  Serial.print(sp.roll);
+  Serial.print("º Yaw: ");
+  Serial.print(sp.yaw);
+  Serial.print("º");
+  Serial.println();
+}
+
+void setup() {
+  #ifdef JOYSTICK1
+	  pinMode(j1.pinButton, INPUT_PULLUP);
+  #endif
+  #ifdef JOYSTICK2
+    pinMode(j2.pinButton, INPUT_PULLUP);
+  #endif
+	Serial.begin(9600);
+}
+
+void loop() {
+  #ifdef JOYSTICK1
+    readJoystick1();
+  #endif
+  #ifdef JOYSTICK2
+    readJoystick2();
+  #endif
+  calculateSetpoints();
+  #ifdef DEBUG
+    #ifdef JOYSTICK1
+      printJoystickData(&j1);
+    #endif
+    #ifdef JOYSTICK2
+      printJoystickData(&j2);
+    #endif
+    printSetpoints();
+  #endif
+	delay(100);
+}
