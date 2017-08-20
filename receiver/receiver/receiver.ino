44a1a3c7f2c0d6651a797d26165b5fc1af923902
xino1010 <daniandujar@gmail.com>
2017-08-20 10:33:00 +0200

    First modification.

--- receiver/receiver/receiver.ino
+++ receiver/receiver/receiver.ino
@@ -4,11 +4,13 @@
 #include "RF24.h"
 #include "PID.h"
 #include <Wire.h>
+#include <Kalman.h>
 
 #define DEBUG
 #ifdef DEBUG
   //#define DEBUG_BMP
   #define DEBUG_IMU
+  //#define DEBUG_KALMAN
   //#define DEBUG_MOTORS
   //#define DEBUG_RADIO
   //#define DEBUG_SONAR
@@ -71,6 +73,8 @@ const byte radioAddress[5] = {'c', 'a', 'n', 'a', 'l'};
 #define MAX_YAW 90
 #define READS_OFFSETS 2000
 #define HEAT_OFFSETS 400
+#define RESTRICT_PITCH // Comment out to restrict roll to ±90deg instead
+
 
 // LED
 #define LED_PIN 7
@@ -102,14 +106,19 @@ const int sizeRadioPIDdata = sizeof(radioPIDdata);
 int cm;
 
 // IMU
-int16_t Acc_rawX, Acc_rawY, Acc_rawZ,Gyr_rawX, Gyr_rawY, Gyr_rawZ, Tmp;
-float Acceleration_angle[3];
-float Gyro_angle[3];
-float Total_angle[3];
-float offsets[2] = {0.0, 0.0};
-float elapsedTime, time, timePrev;
-float rad_to_deg = 180/3.141592654;
+float offsets[3] = {0.0, 0.0, 0.0}; // Pitch, Roll, Yaw
+float Total_angle[3] = {0.0, 0.0, 0.0}; // Pitch, Roll, Yaw
 float currentPitch, currentRoll, currentYaw;
+Kalman kalmanX;
+Kalman kalmanY;
+double accX, accY, accZ;
+double gyroX, gyroY, gyroZ;
+int16_t tempRaw;
+double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+double compAngleX, compAngleY; // Calculated angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+uint32_t timer;
+uint8_t i2cData[14]; // Buffer for I2C data
 
 int myAbs(int value) {
   return value > 0 ? value : value * -1;
@@ -450,41 +459,148 @@ void disableLED() {
 
 
 // IMU
-void getAngles(bool useOffsets) {
-  timePrev = time;  // the previous time is stored before the actual time read
-  time = millis();  // actual time read
-  elapsedTime = (time - timePrev) / 1000;
-   
-  Wire.beginTransmission(0x68);
-  Wire.write(0x3B); //Ask for the 0x3B register- correspond to AcX
-  Wire.endTransmission(false);
-  Wire.requestFrom(0x68,14,true);     
-  Acc_rawX = Wire.read() << 8 | Wire.read(); //each value needs two registres
-  Acc_rawY = Wire.read() << 8 | Wire.read();
-  Acc_rawZ = Wire.read() << 8 | Wire.read();
-  Tmp = Wire.read() << 8 | Wire.read();
-  Gyr_rawX = Wire.read() << 8 | Wire.read(); //Once again we shif and sum
-  Gyr_rawY = Wire.read() << 8 | Wire.read();
-  Gyr_rawZ = Wire.read() << 8 | Wire.read();
-     
-  /*---X---*/
-  Acceleration_angle[0] = atan((Acc_rawY/16384.0)/sqrt(pow((Acc_rawX/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
-  /*---Y---*/
-  Acceleration_angle[1] = atan(-1*(Acc_rawX/16384.0)/sqrt(pow((Acc_rawY/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
-  /*---X---*/
-  Gyro_angle[0] = Gyr_rawX / 131.0; 
-  /*---Y---*/
-  Gyro_angle[1] = Gyr_rawY / 131.0;
-  /*---Z---*/
-  Gyro_angle[2] = Gyr_rawZ / 131.0;
+const uint8_t IMUAddress = 0x68; // AD0 is logic low on the PCB
+const uint16_t I2C_TIMEOUT = 1000; // Used to check for errors in I2C communication
 
-  /*---X axis angle---*/
-  Total_angle[0] = 0.98 * (Total_angle[0] + Gyro_angle[0] * elapsedTime) + 0.02 * Acceleration_angle[0];
-  /*---Y axis angle---*/
-  Total_angle[1] = 0.98 * (Total_angle[1] + Gyro_angle[1] * elapsedTime) + 0.02 * Acceleration_angle[1];
-  /*---Z axis angles---*/
-  //Total_angle[2] = Total_angle[2] + Gyro_angle[2] * elapsedTime;
-  Total_angle[2] = Gyro_angle[2];
+uint8_t i2cWrite(uint8_t registerAddress, uint8_t data, bool sendStop) {
+  return i2cWrite(registerAddress, &data, 1, sendStop); // Returns 0 on success
+}
+
+uint8_t i2cWrite(uint8_t registerAddress, uint8_t *data, uint8_t length, bool sendStop) {
+  Wire.beginTransmission(IMUAddress);
+  Wire.write(registerAddress);
+  Wire.write(data, length);
+  uint8_t rcode = Wire.endTransmission(sendStop); // Returns 0 on success
+  if (rcode) {
+    Serial.print(F("i2cWrite failed: "));
+    Serial.println(rcode);
+  }
+  return rcode; // See: http://arduino.cc/en/Reference/WireEndTransmission
+}
+
+uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
+  uint32_t timeOutTimer;
+  Wire.beginTransmission(IMUAddress);
+  Wire.write(registerAddress);
+  uint8_t rcode = Wire.endTransmission(false); // Don't release the bus
+  if (rcode) {
+    Serial.print(F("i2cRead failed: "));
+    Serial.println(rcode);
+    return rcode; // See: http://arduino.cc/en/Reference/WireEndTransmission
+  }
+  Wire.requestFrom(IMUAddress, nbytes, (uint8_t)true); // Send a repeated start and then release the bus after reading
+  for (uint8_t i = 0; i < nbytes; i++) {
+    if (Wire.available())
+      data[i] = Wire.read();
+    else {
+      timeOutTimer = micros();
+      while (((micros() - timeOutTimer) < I2C_TIMEOUT) && !Wire.available());
+      if (Wire.available())
+        data[i] = Wire.read();
+      else {
+        Serial.println(F("i2cRead timeout"));
+        return 5; // This error value is not already taken by endTransmission
+      }
+    }
+  }
+  return 0; // Success
+}
+
+void getAngles(bool useOffsets) {
+
+  /* Update all the values */
+  while (i2cRead(0x3B, i2cData, 14));
+  accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
+  accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
+  accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
+  tempRaw = (int16_t)((i2cData[6] << 8) | i2cData[7]);
+  gyroX = (int16_t)((i2cData[8] << 8) | i2cData[9]);
+  gyroY = (int16_t)((i2cData[10] << 8) | i2cData[11]);
+  gyroZ = (int16_t)((i2cData[12] << 8) | i2cData[13]);;
+
+  double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+  timer = micros();
+
+  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // It is then converted from radians to degrees
+  #ifdef RESTRICT_PITCH // Eq. 25 and 26
+    double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+    double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+  #else // Eq. 28 and 29
+    double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+    double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+  #endif
+
+  double gyroXrate = gyroX / 131.0; // Convert to deg/s
+  double gyroYrate = gyroY / 131.0; // Convert to deg/s
+
+  #ifdef RESTRICT_PITCH
+    // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+    if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+      kalmanX.setAngle(roll);
+      compAngleX = roll;
+      kalAngleX = roll;
+      gyroXangle = roll;
+    }
+    else {
+      kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+    }
+  
+    if (abs(kalAngleX) > 90) {
+      gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+    }
+    kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+  #else
+    // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+    if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
+      kalmanY.setAngle(pitch);
+      compAngleY = pitch;
+      kalAngleY = pitch;
+      gyroYangle = pitch;
+    }
+    else {
+      kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
+    }
+  
+    if (abs(kalAngleY) > 90) {
+      gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
+    }
+    kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+  #endif
+
+  gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
+  gyroYangle += gyroYrate * dt;
+
+  compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
+  compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
+
+  // Reset the gyro angle when it has drifted too much
+  if (gyroXangle < -180 || gyroXangle > 180) {
+    gyroXangle = kalAngleX;
+  }
+  if (gyroYangle < -180 || gyroYangle > 180) {
+    gyroYangle = kalAngleY;
+  }
+
+  #ifdef DEBUG_KALMAN
+    Serial.print("Pitch: ");
+    Serial.print(pitch); Serial.print("\t");
+    Serial.print(gyroYangle); Serial.print("\t");
+    Serial.print(compAngleY); Serial.print("\t");
+    Serial.print(kalAngleY); Serial.print("\t");
+  
+    Serial.print("Roll: ");
+    Serial.print(roll); Serial.print("\t");
+    Serial.print(gyroXangle); Serial.print("\t");
+    Serial.print(compAngleX); Serial.print("\t");
+    Serial.println(kalAngleX); Serial.print("\t");
+  #endif
+
+  Total_angle[1] = kalAngleY; // Pitch
+  Total_angle[0] = kalAngleX; // Roll
+
+  delay(2);
 
   if (useOffsets) {
     currentPitch = Total_angle[1] + offsets[1];
@@ -526,10 +642,48 @@ void calculateOffsets() {
 }
 
 void initMPU() {
-  Wire.beginTransmission(0x68);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
+  i2cData[0] = 7; // Set the sample rate to 1000Hz - 8kHz/(7+1) = 1000Hz
+  i2cData[1] = 0x00; // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
+  i2cData[2] = 0x00; // Set Gyro Full Scale Range to ±250deg/s
+  i2cData[3] = 0x00; // Set Accelerometer Full Scale Range to ±2g
+  while (i2cWrite(0x19, i2cData, 4, false)); // Write to all four registers at once
+  while (i2cWrite(0x6B, 0x01, true)); // PLL with X axis gyroscope reference and disable sleep mode
+
+  while (i2cRead(0x75, i2cData, 1));
+  if (i2cData[0] != 0x68) { // Read "WHO_AM_I" register
+    #ifdef DEBUG_IMU
+      Serial.print(F("Error reading sensor"));
+    #endif
+    while (1);
+  }
+
+  delay(100); // Wait for sensor to stabilize
+
+  /* Set kalman and gyro starting angle */
+  while (i2cRead(0x3B, i2cData, 6));
+  accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
+  accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
+  accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
+
+  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // It is then converted from radians to degrees
+  #ifdef RESTRICT_PITCH // Eq. 25 and 26
+    double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+    double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+  #else // Eq. 28 and 29
+    double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+    double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+  #endif
+
+  kalmanX.setAngle(roll); // Set starting angle
+  kalmanY.setAngle(pitch);
+  gyroXangle = roll;
+  gyroYangle = pitch;
+  compAngleX = roll;
+  compAngleY = pitch;
+
+  timer = micros();
 }
 
 void initVars() {
@@ -611,33 +765,7 @@ void initVars() {
 
 }
 
-void setup() {
-  Wire.begin();
-  
-  #ifdef DEBUG
-    Serial.begin(250000);
-    while (!Serial) {}
-  #endif
-  
-  initVars();
-
-  // Init MPU9250
-  initMPU();
-
-  delay(500);
-
-  time = millis(); //Start counting time in milliseconds
-  calculateOffsets();
-  enableLED();
-
-  // Connect Motors
-  connectMotors();
-  delay(50);
-
-  // Send a minimum signal to prepare the motors
-  armMotors();
-  delay(50);
-
+void countDown() {
   int i = 9;
   #ifdef DEBUG
     Serial.println("Countdown: ");
@@ -653,11 +781,46 @@ void setup() {
     enableLED();
     i--;
   }
-	#ifdef DEBUG
-	  Serial.println("Quadcopter initialized");
+  #ifdef DEBUG
+    Serial.println("Quadcopter initialized");
   #endif
+}
 
-  disableLED();
+void setup() {
+  Wire.begin();
+  #if ARDUINO >= 157
+    Wire.setClock(400000UL); // Set I2C frequency to 400kHz
+  #else
+    TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
+  #endif
+  
+  #ifdef DEBUG
+    Serial.begin(115200);
+    while (!Serial) {}
+  #endif
+  
+  initVars();
+
+  // Init MPU9250
+  initMPU();
+
+  delay(500);
+
+  //calculateOffsets();
+  //enableLED();
+
+  // Connect Motors
+  //connectMotors();
+  //delay(50);
+
+  // Send a minimum signal to prepare the motors
+  //armMotors();
+  //delay(50);
+
+  // Wait a while
+  //countDown();
+
+  //disableLED();
   
 }
 
@@ -665,12 +828,12 @@ void loop() {
 
   #ifdef NORMAL_MODE
   	// Read data from radio
-  	updateRadioInfo();
+  	//updateRadioInfo();
   #endif
 
   #ifdef CALIBRATION_MODE
     // Read constants of kpi from radio
-    updatePIDInfo();
+    //updatePIDInfo();
   #endif
 
   // Read angles from sensor
@@ -678,14 +841,14 @@ void loop() {
 
   if (cm == CONTROL_MODE_OFF) {
     // Set minim velocity to all motors
-    stopMotors();
+    //stopMotors();
   }
   else {
     // Calculate velocities of each motor depending of ControlMode
-    calculateVelocities();
+    //calculateVelocities();
   }
 
   // Send velocities to motors
-  updateMotorsVelocities();
+  //updateMotorsVelocities();
 
 }
